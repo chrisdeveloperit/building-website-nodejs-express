02151072b03505bdbfcfc78fc61f1445a0e344c1
Chris Collier <christopher.collier.ctr@gyver.com>
2023-02-23 13:48:43 -0800

    Add routing and services

--- server.js
+++ server.js
@@ -2,20 +2,25 @@
 /* eslint linebreak-style: ["error", "windows"] */
 
 const express = require('express');
+const path = require('path');
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
 
+// Instantiate Services
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
+
+const routes = require('./routes');
 const app = express();
 const port = 3000;
-const path = require('path');
-// ****** ROUTES ******/
-app.use(express.static(path.join((__dirname, './static'))));
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/index.html'));
-});
 
-app.get('/speakers', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+// ****** ROUTES ******/
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.use(express.static(path.join((__dirname, './static'))));
+app.use('/', routes({ feedbackService, speakerService }));
 
 app.listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
+	console.log(`Express server listening on port ${port}`);
 });
